fcedc6d9244b92f044d7b8841a7e76d5c2006bcd
Winter Yang <aiyld@hotmail.com>
2024-10-16 23:42:42 +0800

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,7 +1,7 @@
 module.exports = {
   list: [{
     "name": "react-next-framework",
-    "path": "aiyld/react-next-framework",
+    "path": "WinterMain/react-next-framework",
     "desc": "React + nextjs + i18n + less + redux + router + eslint full framework."
   }]
 }
